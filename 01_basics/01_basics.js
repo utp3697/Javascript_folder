d3be9820c37fcd849d7bc0fc5017ca27b0e6eabc
utp3697 <147048582+utp3697@users.noreply.github.com>
2026-06-11 08:31:22 +0000

    container config done

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ahireutpreksha@gmail.com"
+var accountPassword = "1234"
+accountCity ="Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "af@sjd.com"
+accountPassword ="1224354"
+accountCity ="Bhutan"
+
+
+
+console.table([accountId, accountEmail , accountPassword , accountCity]);
